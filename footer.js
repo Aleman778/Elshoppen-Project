d38102efeda4042062c8580c790b98f3e1e7762b
Alexander Mennborg <alexander.mennborg@gmail.com>
2018-11-13 21:58:05 +0100

    Updated footer

---
+++ footer.js
@@ -0,0 +1,13 @@
+$(document).ready(fixFooter);
+$(window).resize(fixFooter);
+$("#navbarSupportedContent").resize(fixFooter);
+
+
+function fixFooter() {
+    var docHeight = $(window).height();
+    var footerHeight = $('#footer').height();
+    var footerTop = $('#footer').position().top + footerHeight;
+
+    if (footerTop < docHeight)
+        $('#footer').css('margin-top', 10 + (docHeight - footerTop) + 'px');
+}
--- footer.php
+++ footer.php
@@ -1,11 +1,46 @@
-<footer>
-  <div class="container">
-    Detta är en footer!
-    Skriv kontakt information, snabblänkar... <br>
-    Hemsida skapad av:<br>
-    Alexander Mennborg<br>
-    Jonas Jarnhäll Sjöman<br>
-    Niklas Lundberg<br>
-    Ruben Asplund
+
+<style>
+  html, body {
+    height: 100%;
+  }
+</style>
+
+<footer id="footer" style="background-color: rgb(25, 25, 25);">
+  <div class="container py-4" style="color: white;">
+    <h2>ELSHOPPEN</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <p>
+            <b>Hemsida skapad av</b><br>
+            Alexander Mennborg<br>
+            Jonas Jarnhäll Sjöman<br>
+            Niklas Lundberg<br>
+            Ruben Asplund
+          </p>
+        </div>
+        <div class="col-sm">
+          <b>Kundtjänst</b><br>
+          <a href="#">Kontakta oss</a><br>
+          <a href="#">Vanliga frågor</a><br>
+          <a href="#">Spåra din leverans</a>
+        </div>
+        <div class="col-sm">
+          <b>Information</b><br>
+          <a href="#">GDPR</a><br>
+          <a href="#">Köpvillkor</a><br>
+          <a href="#">...</a>
+        </div>
+        <div class="col-sm">
+          <b>Om ELSHOPPEN</b><br>
+          <a href="#">Om oss</a><br>
+          <a href="#">Söka jobb</a><br>
+          <a href="#">...</a>
+        </div>
+      </div>
+    </div>
+    <p>
+      @2018 Elshoppen AB. All rights reserved.
+    </p>
   </div>
 </footer>
