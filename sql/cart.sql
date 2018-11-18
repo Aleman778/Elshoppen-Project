38ef761396faff63570c39b5c28063419f32574d
Niklas Lundberg <nicke.l@telia.com>
2018-11-18 17:31:46 +0100

    Lagt till fler sql script

--- sql/cart.sql
+++ sql/cart.sql
@@ -2,5 +2,6 @@ CREATE TABLE CART (
 	customer_id INT(30) UNSIGNED NOT NULL,
 	product_id INT(30) UNSIGNED NOT NULL,
 	quantity INT(100) UNSIGNED NOT NULL,
-    PRIMARY KEY ( customer_id )
+    PRIMARY KEY ( customer_id ),
+	FOREIGN KEY (customer_id) REFERENCES CUSTOMERS (id)
 );
