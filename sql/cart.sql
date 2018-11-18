1e895c7571163a8151400285bc889f43a0abe3e2
Niklas Lundberg <nicke.l@telia.com>
2018-11-18 18:06:35 +0100

    Fixat sql koden för tabelerna

--- sql/cart.sql
+++ sql/cart.sql
@@ -2,6 +2,7 @@ CREATE TABLE CART (
 	customer_id INT(30) UNSIGNED NOT NULL,
 	product_id INT(30) UNSIGNED NOT NULL,
 	quantity INT(100) UNSIGNED NOT NULL,
-    PRIMARY KEY ( customer_id ),
-	FOREIGN KEY ( customer_id ) REFERENCES CUSTOMERS (id)
+    PRIMARY KEY ( customer_id, product_id ),
+	FOREIGN KEY ( customer_id ) REFERENCES CUSTOMERS (id),
+	FOREIGN KEY ( product_id ) REFERENCES PRODUCTS ( id )
 );
--- sql/orders.sql
+++ sql/orders.sql
@@ -1,11 +1,9 @@
 create table ORDERS (
     id INT NOT NULL AUTO_INCREMENT,
     customer_id INT,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
     address VARCHAR(100) NOT NULL,
     price FLOAT NOT NULL,
     email VARCHAR(100) NOT NULL,
-    PRIMARY KEY ( id, product_id ),
+    PRIMARY KEY ( id ),
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS (id)
 );
