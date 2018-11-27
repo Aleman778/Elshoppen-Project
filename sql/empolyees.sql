5f7628a967dccfac0effa0ff62fd740c3d638523
lancer17NE <nejosj@gmail.com>
2018-11-27 10:06:58 +0100

    Anställdatabell
    allt data som finns i customers finns i denna tabell också

---
+++ sql/empolyees.sql
@@ -0,0 +1,13 @@
+create table EMPLOYEES (
+    lastname VARCHAR(40) NOT NULL,
+    firstname VARCHAR(40) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    gender CHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    removed TINYINT(1) NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    phone_number INT,
+    address VARCHAR(100),
+    role VARCHAR(12)
+);
