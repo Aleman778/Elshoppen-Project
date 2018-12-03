b6b3cd9cf6185399b420b9a5fd0216dd4fb38f2f
Niklas Lundberg <nicke.l@telia.com>
2018-12-03 15:28:58 +0100

    datum och tid finns nu i orders och comments

--- sql/orders.sql
+++ sql/orders.sql
@@ -1,8 +1,9 @@
 create table ORDERS (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    customer_id INT UNSIGNED,
+    customer_id INT UNSIGNED NOT NULL,
     address VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
+    time DATETIME NOT NULL,
     PRIMARY KEY ( id ),
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS (id)
 );
