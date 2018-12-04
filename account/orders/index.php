325419b892d2976598b020e8791f6c147f343f20
Niklas Lundberg <nicke.l@telia.com>
2018-12-04 13:15:00 +0100

    Dina beställningar GUI

--- account/orders/index.php
+++ account/orders/index.php
@@ -13,8 +13,6 @@
   $db = new MySQL();
   $sql = "SELECT * From ORDERS WHERE customer_id LIKE $customer_id";
   $orders =  $db->fetchAll($sql);
-  $sql = "SELECT * From ORDERS_PRODUCTS"; // WHERE order_id in (SELECT id FROM ORDERS WHERE customer_id LIKE $customer_id)";
-  $items =  $db->fetchAll($sql);
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,8 +27,34 @@
   <div id="main" class="container">
     <h1>Mina beställningar</h1>
     <div class="row">
-      <?php  
-        var_dump($items);?>
+      
+      <?php foreach ($orders as $order) { 
+        $id = $order["id"];
+        $items =  $db->fetchAll("SELECT product_id, quantity FROM ORDERS_PRODUCTS WHERE order_id = $id");
+      ?>
+
+        <div class="card card-item m-2" style="width: 16rem;">
+            <div class="card-body">
+                <h5 class="card-title" style="max-height: 48px; overflow: hidden;">
+                    <b>Order nummer: </b> <?php echo $id ?>
+                </h5>
+                  <p><b>Address: </b> <?php echo $order[2] ?> </p>
+                  <p><b>E-mail: </b> <?php echo $order[3] ?> </p>
+                <p class="card-subtitle pb-2"><b>Din Beställning:</b></p>
+                  <?php foreach ($items as $item) { ?>
+                    <li>
+                      <?php 
+                        $prod =  $db->fetch("SELECT name FROM PRODUCTS WHERE id = $item[0]");
+                        echo (string) $prod[0];
+                      ?>
+                      <p> 
+                      <b>Antal: </b><?php  echo (string) $item[1] ?></p> 
+                    </li>
+                  <?php } ?>
+            </div>
+        </div>
+      <?php } ?>
+
     </div>
   </div>
 
