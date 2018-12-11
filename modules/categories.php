ef70b559f9c65f895476cfef31f5f2c8ba2fab26
Alexander Mennborg <alexander.mennborg@gmail.com>
2018-12-11 22:49:06 +0100

    Added cat-item class to separate other items

--- modules/categories.php
+++ modules/categories.php
@@ -1,16 +1,16 @@
 <!-- graphical UI showing a sample of categories on the front -->
 <div id="categoryList" class="list-group" >
-  <a id="categoriesItem"  class="list-group-item"> Kategorier </a>
-  <a href="/product/category?name=Gaming"  class=" list-group-item list-group-item-action">Gaming</a>
-  <a href="/product/category?name=Tv och bild" class="list-group-item list-group-item-action">Tv och bild</a>
-  <a href="/product/category?name=Mobiltelefoner" class="list-group-item list-group-item-action">Mobiltelefoner</a>
-  <a href="/product/category?name=Datorer" class="list-group-item list-group-item-action">Datorer</a>
-  <a href="/product/category?name=Vitvaror" class="list-group-item list-group-item-action">Vitvaror</a>
-  <a href="/product/category?name=Hem och Hushåll" class="list-group-item list-group-item-action">Hem och hushåll</a>
-  <a href="/product/category?name=Personvård" class="list-group-item list-group-item-action">Personvård</a>
-  <a href="/product/category?name=Foto och video" class="list-group-item list-group-item-action">Foto och video</a>
-  <a href="/product/category?name=Smarta hem" class="list-group-item list-group-item-action">Smart hem</a>
-  <a href="/product/category?name=Wearables och träning" class="list-group-item list-group-item-action">Wearables och träning</a>
-  <a href="/product/category?name=Ljud och hi-fi" class="list-group-item list-group-item-action">Ljud och hi-fi</a>
+  <a id="categoriesItem"  class="cat-item list-group-item"> Kategorier </a>
+  <a href="/product/category?name=Gaming"  class="cat-item list-group-item list-group-item-action">Gaming</a>
+  <a href="/product/category?name=Tv och bild" class="cat-item list-group-item list-group-item-action">Tv och bild</a>
+  <a href="/product/category?name=Mobiltelefoner" class="cat-item list-group-item list-group-item-action">Mobiltelefoner</a>
+  <a href="/product/category?name=Datorer" class="cat-item list-group-item list-group-item-action">Datorer</a>
+  <a href="/product/category?name=Vitvaror" class="cat-item list-group-item list-group-item-action">Vitvaror</a>
+  <a href="/product/category?name=Hem och Hushåll" class="cat-item list-group-item list-group-item-action">Hem och hushåll</a>
+  <a href="/product/category?name=Personvård" class="cat-item list-group-item list-group-item-action">Personvård</a>
+  <a href="/product/category?name=Foto och video" class="cat-item list-group-item list-group-item-action">Foto och video</a>
+  <a href="/product/category?name=Smarta hem" class="cat-item list-group-item list-group-item-action">Smart hem</a>
+  <a href="/product/category?name=Wearables och träning" class="cat-item list-group-item list-group-item-action">Wearables och träning</a>
+  <a href="/product/category?name=Ljud och hi-fi" class="cat-item list-group-item list-group-item-action">Ljud och hi-fi</a>
 
 </div>
