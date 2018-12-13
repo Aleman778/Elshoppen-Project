32c9f49a3a3598f3efe68023b2c3f1963f66792d
Niklas Lundberg <nicke.l@telia.com>
2018-12-13 16:52:30 +0100

    Fixa email när man tar bot konto som admin

--- admin/users/list/delete.php
+++ admin/users/list/delete.php
@@ -7,19 +7,19 @@
     $db = new MySQL();
     $db->query("START TRANSACTION");
     $sql = "UPDATE CUSTOMERS SET firstname = :fname, lastname = :lname, 
-            birth_date = :bday, password = :psw, removed = :rem,
+            birth_date = :bday, email = :email, password = :psw, removed = :rem,
             phone_number = :mobile_number, address = :address where id = :id;";
     $pid = $_GET["id"];
     $p = $db->prepare($sql);
     $p->execute(array(  "fname" => "REMOVED",
                         "lname" => "REMOVED",
                         "bday" => "1000-01-01",
+                        "email" => "REMOVED<?php exho $pid ?>@REMOVED",
                         "mobile_number" => NULL,
                         "address" => NULL,
                         "id" => $pid,
                         "psw"=> "REMOVED",
                         "rem" => "1"));
     $db->query("COMMIT");
-
     header("Location: http://localhost/admin/users/list/");
 ?>
